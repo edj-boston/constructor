377ab0c566f2a9824f95c14defae32ab9e5b5efa
Eric Johnson <edj.boston@gmail.com>
2015-05-23 14:50:51 -0700

    Adding length, amplitude, and phase to the edge model. Also tests.

--- src/models/Edge.js
+++ src/models/Edge.js
@@ -2,7 +2,7 @@ var uuid = require('uuid'),
 	validateUUID = require('../../lib/validateUUID.js');
 
 
-var Edge = function(id, type, vid1, vid2) {
+var Edge = function(id, type, vid1, vid2, length, amplitude, phase) {
 
 	// Validate the id
 	if( validateUUID(id) ) {
@@ -32,8 +32,53 @@ var Edge = function(id, type, vid1, vid2) {
 		throw new Error('vid2 must be a valid, type 4 UUID');
 	}
 
+	// Validate the length
+	if( typeof length == 'number' ) {
+		this.length = length;
+	} else {
+		throw new Error('length must be numeric');
+	}
+
+	if( type == 'muscle' ) {
+
+		if( this.validateAmpOrPhase(amplitude) ) {
+			this.amplitude = amplitude;
+		} else {
+			throw new Error('amplitude must be a number between 0 and 1 (inclusive)');
+		}
+
+		if( this.validateAmpOrPhase(phase) ) {
+			this.phase = phase;
+		} else {
+			throw new Error('phase must be a number between 0 and 1 (inclusive)');
+		}
+
+	}
+
 	return this;
 }
 
+Edge.prototype.validateAmpOrPhase = function(val) {
+	return ( typeof val == 'number' && val >= 0  && val <=1 );
+}
+
+Edge.prototype.springify = function() {
+	this.type = 'spring';
+	delete this.amplitude;
+	delete this.phase;
+
+	return this;
+}
+
+Edge.prototype.musclize = function(amplitude, phase) {
+
+	if( this.validateAmpOrPhase(amplitude) && this.validateAmpOrPhase(phase) ) {
+		this.type = 'muscle';
+		this.amplitude = amplitude;
+		this.phase = phase;
+	}
+
+	return this;	
+}
 
 module.exports = Edge;
--- test/Edge.js
+++ test/Edge.js
@@ -29,4 +29,46 @@ describe('Edge', function() {
 		}).should.throw('vid2 must be a valid, type 4 UUID');
 	});
 
+	it('should cause an error if the length is not numeric', function() {
+		(function() {
+			var e = new Edge(uuid.v4(), 'spring', uuid.v4(), uuid.v4(), null);
+		}).should.throw('length must be numeric');
+	});
+
+	it('should cause an error if the amplitude is invalid', function() {
+		(function() {
+			var e = new Edge(uuid.v4(), 'muscle', uuid.v4(), uuid.v4(), 0, null, 0);
+		}).should.throw('amplitude must be a number between 0 and 1 (inclusive)');
+	});
+
+	it('should cause an error if the phase is invalid', function() {
+		(function() {
+			var e = new Edge(uuid.v4(), 'muscle', uuid.v4(), uuid.v4(), 0, 0, null);
+		}).should.throw('phase must be a number between 0 and 1 (inclusive)');
+	});
+
+	describe('#springify', function() {
+
+		it('should change the type to spring and delete amplitude and velocity', function() {
+			var e = new Edge(uuid.v4(), 'muscle', uuid.v4(), uuid.v4(), 0, 0, 0);
+			e.springify();
+			e.type.should.equal('spring');
+			should.not.exist(e.amplitude);
+			should.not.exist(e.phase);
+		});
+
+	});
+
+	describe('#musclize', function() {
+
+		it('should change the type to muscle and create amplitude and velocity', function() {
+			var e = new Edge(uuid.v4(), 'spring', uuid.v4(), uuid.v4(), 0, 0, 0);
+			e.musclize(0, 0);
+			e.type.should.equal('muscle');
+			should.exist(e.amplitude);
+			should.exist(e.phase);
+		});
+
+	});
+
 });
