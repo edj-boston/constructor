27b9815149471438b6c2f72d2099f703481f0c46
Eric Johnson <edj.boston@gmail.com>
2015-05-26 22:10:27 -0700

    Changing friction to f, more validation and tests

--- src/models/Model.js
+++ src/models/Model.js
@@ -2,22 +2,22 @@ var uuid = require('uuid'),
 	validateUUID = require('../../lib/validateUUID.js');
 
 
-var Model = function(autoReverse, comment, friction, gravity, gravityDirection, height, name, k, friction, reflection, amplitude, direction, phase, speed, width) {
+var Model = function(autoReverse, comment, f, gravity, gravityDirection, height, name, k, friction, reflection, amplitude, direction, phase, speed, width) {
 	this.autoReverse = autoReverse;
 	this.comment = comment;
-	this.friction = friction;
+	this.f = f;
 	this.gravity = gravity;
 	this.gravityDirection = gravityDirection;
 	this.height = height;
 	this.name = name;
 	this.k = k;
-	this.friction = friction;
-	this.reflection = reflection;
+
+	/*this.reflection = reflection;
 	this.amplitude = amplitude;
 	this.direction = direction;
 	this.phase = phase;
 	this.speed = speed;
-	this.width = width;
+	this.width = width;*/
 
 	this.validate();
 
@@ -32,11 +32,33 @@ Model.prototype.validate = function() {
 	// Validate the comment
 	if( typeof this.comment != 'string' ) throw new Error('comment must be a string');
 
-	// Validate the friction value
-	if( typeof this.friction != 'number' || this.friction < 0  || this.friction > 1 ) {
-		throw new Error('`friction` must be a number between 0 and 1 (inclusive)');
+	// Validate the f value
+	if( typeof this.f != 'number' || this.f < 0 || this.f > 1 ) {
+		throw new Error('`f` must be a number between 0 and 1 (inclusive)');
 	}
 
+	// Validate the gravity value
+	if( typeof this.gravity != 'number' || this.gravity < 0  || this.gravity > 1 ) {
+		throw new Error('`gravity` must be a number between 0 and 1 (inclusive)');
+	}
+
+	// Validate the gravityDirection
+	if( this.gravityDirection != 1 && this.gravityDirection != -1 ) {
+		throw new Error('`gravityDirection` must be either -1 or 1');
+	}
+
+	// Validate the height value
+	if( typeof this.height != 'number' || this.height <= 0 ) {
+		throw new Error('`height` must be a number greater than 0');
+	}
+
+	// Validate the name
+	if( typeof this.name != 'string' ) throw new Error('name must be a string');
+
+	// Validate the k value
+	if( typeof this.k != 'number' || this.k < 0 || this.k > 1 ) {
+		throw new Error('`k` must be a number between 0 and 1 (inclusive)');
+	}
 }
 
 module.exports = Model;
--- test/Model.js
+++ test/Model.js
@@ -17,10 +17,40 @@ describe('Model', function() {
 		}).should.throw('comment must be a string');
 	});
 
-	it('should thow an error for an invalid friction value', function() {
+	it('should thow an error for an invalid f value', function() {
 		(function() {
 			var m = new Model(true, "comment", -1);
-		}).should.throw('`friction` must be a number between 0 and 1 (inclusive)');
+		}).should.throw('`f` must be a number between 0 and 1 (inclusive)');
+	});
+
+	it('should thow an error for an invalid gravity value', function() {
+		(function() {
+			var m = new Model(true, "comment", 0, -1);
+		}).should.throw('`gravity` must be a number between 0 and 1 (inclusive)');
+	});
+
+	it('should thow an error for an invalid gravityDirection value', function() {
+		(function() {
+			var m = new Model(true, "comment", 0, 0, 0);
+		}).should.throw('`gravityDirection` must be either -1 or 1');
+	});
+
+	it('should thow an error for an invalid height value', function() {
+		(function() {
+			var m = new Model(true, "comment", 0, 0, 1, 0);
+		}).should.throw('`height` must be a number greater than 0');
+	});
+
+	it('should thow an error for an invalid name', function() {
+		(function() {
+			var m = new Model(true, "comment", 0, 0, 1, 1, null);
+		}).should.throw('name must be a string');
+	});
+
+	it('should thow an error for an invalid k value', function() {
+		(function() {
+			var m = new Model(true, "comment", 0, 0, 1, 1, "name", -1);
+		}).should.throw('`k` must be a number between 0 and 1 (inclusive)');
 	});
 
 });
