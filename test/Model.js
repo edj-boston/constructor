de7758fb53ab39b322dc8e3e8170a4c1bc7599bf
Eric Johnson <edj.boston@gmail.com>
2015-05-29 10:44:30 -0700

    Additional tests for ranges of values

--- test/Model.js
+++ test/Model.js
@@ -17,15 +17,27 @@ describe('Model', function() {
 		}).should.throw('comment must be a string');
 	});
 
-	it('should thow an error for an invalid f value', function() {
+	it('should thow an error for an f value less than zero', function() {
 		(function() {
 			var m = new Model(true, "comment", -1);
 		}).should.throw('`f` must be a number between 0 and 1 (inclusive)');
 	});
 
-	it('should thow an error for an invalid gravity value', function() {
+	it('should thow an error for an f value greater than 1', function() {
+		(function() {
+			var m = new Model(true, "comment", 1.5);
+		}).should.throw('`f` must be a number between 0 and 1 (inclusive)');
+	});
+
+	it('should thow an error for a gravity value less than zero', function() {
 		(function() {
 			var m = new Model(true, "comment", 0, -1);
+		}).should.throw('`gravity` must be a number between 0 and 1 (inclusive)');
+	});
+
+	it('should thow an error for a gravity value greater than 1', function() {
+		(function() {
+			var m = new Model(true, "comment", 0, 1.5);
 		}).should.throw('`gravity` must be a number between 0 and 1 (inclusive)');
 	});
 
@@ -37,7 +49,13 @@ describe('Model', function() {
 
 	it('should thow an error for an invalid height value', function() {
 		(function() {
-			var m = new Model(true, "comment", 0, 0, 1, 0);
+			var m = new Model(true, "comment", 0, 0, 1, "string");
+		}).should.throw('`height` must be a number greater than 0');
+	});
+
+	it('should thow an error for a height value less than zero', function() {
+		(function() {
+			var m = new Model(true, "comment", 0, 0, 1, -1);
 		}).should.throw('`height` must be a number greater than 0');
 	});
 
@@ -47,9 +65,15 @@ describe('Model', function() {
 		}).should.throw('name must be a string');
 	});
 
-	it('should thow an error for an invalid k value', function() {
+	it('should thow an error for a k value less than zero', function() {
 		(function() {
 			var m = new Model(true, "comment", 0, 0, 1, 1, "name", -1);
+		}).should.throw('`k` must be a number between 0 and 1 (inclusive)');
+	});
+
+	it('should thow an error for a k value greater than 1', function() {
+		(function() {
+			var m = new Model(true, "comment", 0, 0, 1, 1, "name", 1.5);
 		}).should.throw('`k` must be a number between 0 and 1 (inclusive)');
 	});
 
