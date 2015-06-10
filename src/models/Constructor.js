9a3eaf904eeb909321158310df1f477a096c0533
Eric Johnson <edj.boston@gmail.com>
2015-06-09 21:56:12 -0700

    Generic loadModel function with tests

--- src/models/Constructor.js
+++ src/models/Constructor.js
@@ -32,9 +32,44 @@ Constructor.prototype.validate = function() {
 
 }
 
-Constructor.prototype.loadJSON = function(json) {
+Constructor.prototype.loadModel = function(arg) {
 
-	var obj = JSON.parse(json);
+	if(arg == null) throw new Error('The argument cannot be null');
+
+	switch( typeof arg ) {
+		case 'string':
+			this.loadModelJSON(arg);
+			break;
+		case 'object':
+			this.loadModelObject(arg);
+			break;
+		default:
+			throw new Error('Cannot load this model');
+			break;
+	}
+
+	return this;
+
+}
+
+Constructor.prototype.loadModelJSON = function(json) {
+
+	try {
+		var obj = JSON.parse(json);
+		this.model = new Model(obj);
+	} catch(e) {
+		throw new Error('Invalid JSON input');
+	}
+
+	return this;
+
+}
+
+Constructor.prototype.loadModelObject = function(obj) {
+
+	if( typeof obj != 'object' || obj == null ) {
+		throw new Error('Argument must be an object');
+	}
 
 	this.model = new Model(obj);
 
--- test/Constructor.js
+++ test/Constructor.js
@@ -5,13 +5,66 @@ var assert = require('assert'),
 
 describe('Constructor', function() {
 
-	describe('#loadJSON', function() {
+	describe('#loadModel', function() {
 
-		it('The options argument is required', function() {
-			var json = fs.readFileSync('test/fixtures/model.json', 'utf-8');
+		it('should reject a null argument', function() {
+			(function() {
+				var c = new Constructor();
+				c.loadModel(null);
+			}).should.throw('The argument cannot be null');
+		});
+
+		it('should detect and load a JSON argument', function() {
 			var c = new Constructor();
-			c.loadJSON(json);
-			console.log(c.model);
+			var json = fs.readFileSync('test/fixtures/model.json', 'utf-8');
+			c.loadModel(json);
+			assert.equal(c.model.name, 'model');
+		});
+
+		it('should detect and load an object argument', function() {
+			var c = new Constructor();
+			var json = fs.readFileSync('test/fixtures/model.json', 'utf-8');
+			var obj = JSON.parse(json);
+			c.loadModel(obj);
+			assert.equal(c.model.name, 'model');
+		});
+
+	});
+
+	describe('#loadModelJSON', function() {
+
+		it('should reject invalid JSON', function() {
+			(function() {
+				var c = new Constructor();
+				var json = '{';
+				c.loadModelJSON(json);
+			}).should.throw('Invalid JSON input');
+		});
+
+		it('should load a mode from a JSON string', function() {
+			var c = new Constructor();
+			var json = fs.readFileSync('test/fixtures/model.json', 'utf-8');
+			c.loadModelJSON(json);
+			assert.equal(c.model.name, 'model');
+		});
+
+	});
+
+	describe('#loadModelObject', function() {
+
+		it('should reject an invalid argument', function() {
+			(function() {
+				var c = new Constructor();
+				c.loadModelObject(null);
+			}).should.throw('Argument must be an object');
+		});
+
+		it('load a model from an object variable', function() {
+			var c = new Constructor();
+			var json = fs.readFileSync('test/fixtures/model.json', 'utf-8');
+			var obj = JSON.parse(json);
+			c.loadModelObject(obj);
+			assert.equal(c.model.name, 'model');
 		});
 
 	});
